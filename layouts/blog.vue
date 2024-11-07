a21ad8d87dd98e2a3b96737eb00f3c3e0932847e
xarn0 <xouli@list.ru>
2024-11-07 23:44:05 +0300

    full

---
+++ layouts/blog.vue
@@ -0,0 +1,246 @@
+<template>
+  <v-app light>
+    <v-app-bar
+      app
+      class="elevation-0 absolute"
+      :class="{ transparent: checkTransparent }"
+      dense
+      tile
+      outlined
+      hide-on-scroll
+      scroll-threshold="55"
+    >
+      <v-app-bar-nav-icon
+        color="#156963"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-btn color=" mx-2" icon><v-icon>mdi-magnify</v-icon></v-btn>
+      <v-btn color=" mx-2" icon><v-icon>mdi-basket</v-icon></v-btn>
+      <v-btn
+        style="color: #156963"
+        @click.stop="dialog2 = true"
+        class="mx-1 pa-3 px-sm-5 transparent font-weight-bold elevation-0 text-body-2 text-md-body-1"
+        >sign up</v-btn
+      >
+      <v-btn
+        color="#156963"
+        class="pa-3 px-sm-5 mx-1 opacity white--text elevation-0 text-body-2 text-md-body-1"
+        @click.stop="dialog = true"
+        >sign in</v-btn
+      >
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-layout class="flex-column" style="height: 100%">
+        <v-flex cols="12">
+          <v-list>
+            <v-list-item-group>
+              <v-list-item>
+                <v-list-item-content> Lorem ipsum </v-list-item-content>
+                <v-icon>mdi-account-box-edit-outline</v-icon>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content> Lorem ipsum2 </v-list-item-content>
+                <v-icon>mdi-access-point</v-icon>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content> Lorem ipsum3 </v-list-item-content>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content> Lorem ipsum4 </v-list-item-content>
+                <v-icon>mdi-alpha-f-circle-outline</v-icon>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content> Lorem ipsum4 </v-list-item-content>
+                <v-icon>mdi-alpha-f-circle-outline</v-icon>
+              </v-list-item>
+              <v-spacer></v-spacer>
+            </v-list-item-group>
+          </v-list>
+          <div class="d-flex justify-center my-5">
+            <v-btn
+              color="orange"
+              class="white--text justify-center"
+              @click="drawer = false"
+            >
+              <h5>закрыть</h5>
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </div>
+        </v-flex>
+        <v-flex
+          class="d-flex align-end justify-center mb-2 flex-wrap flex-md-nowrap"
+        >
+          <v-btn color="primary" @click.stop="dialog = true">Вход</v-btn>
+          <v-btn color="green white--text ml-md-2" @click.stop="dialog2 = true"
+            >Регистрация</v-btn
+          >
+          <!-- <v-btn>выход</v-btn> -->
+        </v-flex>
+      </v-layout>
+    </v-navigation-drawer>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="green white--text lighten-2">
+          <div class="flex-column d-flex">
+            <p class="text-h5">Вход</p>
+            <span class="text-body-2"> В аккаунт </span>
+          </div>
+          <v-spacer />
+
+          <v-icon color="white" @click="dialog = false">mdi-close</v-icon>
+        </v-card-title>
+
+        <v-card elevation="0">
+          <v-container>
+            <v-form>
+              <v-text-field :counter="15" label="Name" required></v-text-field>
+              <v-text-field
+                type="password"
+                label="password"
+                required
+              ></v-text-field> </v-form
+          ></v-container>
+        </v-card>
+
+        <v-divider></v-divider>
+        <v-spacer></v-spacer>
+        <v-card-actions class="text-center d-flex justify-center">
+          <v-btn color="primary text-h4 " text @click="dialog = false">
+            Вход</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialog2" width="500">
+      <v-card>
+        <v-card-title
+          class="text-h5 purple lighten-2 white--text justify-center lighten-2 flex-column"
+        >
+          Регистрация
+        </v-card-title>
+
+        <v-card elevation="0">
+          <v-container>
+            <v-form>
+              <v-text-field :counter="15" label="Name" required></v-text-field>
+              <v-text-field
+                type="password"
+                label="password"
+                required
+              ></v-text-field>
+              <v-text-field
+                type="password"
+                label="password"
+                required
+              ></v-text-field> </v-form
+          ></v-container>
+        </v-card>
+
+        <v-divider></v-divider>
+        <v-spacer></v-spacer>
+        <v-card-actions class="text-center d-flex justify-center">
+          <v-btn color="primary text-h4 " text @click="dialog2 = false">
+            Вход</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-main>
+      <div class="d-flex mr-md-10" style="gap: 40px">
+        <nuxt />
+      </div>
+    </v-main>
+    <footer class="mt-10">
+      <v-divider class="pa-4"></v-divider>
+      <v-container>
+        <v-row class="justify-space-between">
+          <v-col cols="6" sm="4" md="4">
+            <v-subheader
+              class="pa-0 text-uppercase font-weight-medium"
+              style="color: #156963; letter-spacing: 3px; font-size: 24px"
+              >plantku</v-subheader
+            >
+            <div>
+              <h5
+                style="color: #333333; font-size: 18px; letter-spacing: 2px"
+                class="mb-2"
+              >
+                Plantku House
+              </h5>
+              <p>
+                Jl. Laksda Adisucipto No.51, Demangan, Kec. Depok, Kota
+                Yogyakarta, Daerah Istimewa Yogyakarta 55282
+              </p>
+            </div>
+          </v-col>
+          <v-col cols="6" sm="3" md="2">
+            <v-list>
+              <v-subheader class="text-h6 font-weight-bold"
+                >Perusahaan</v-subheader
+              >
+              <v-list-item-group>
+                <v-list-item>Tentang Kami</v-list-item>
+                <v-list-item>Hubungi Kami2</v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+          <v-col cols="6" md="2">
+            <v-list>
+              <v-subheader class="text-h6 font-weight-bold">Produk</v-subheader>
+              <v-list-item-group>
+                <v-list-item>Tanaman</v-list-item>
+                <v-list-item>Tanaman Lain</v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-col>
+
+          <v-col cols="6" md="3">
+            <v-form>
+              <v-subheader
+                class="pl-0 text-capitalize black--text font-weight-medium text-body-1"
+                >Berlangganan Email Kami</v-subheader
+              >
+              <v-text-field
+                outlined
+                class="pa-0"
+                dense
+                tile
+                label="Masukan Alamat Email"
+                style="border-color: #f2f2f2"
+              ></v-text-field>
+              <v-btn color="#156963" class="white--text text-capitalize"
+                >submit</v-btn
+              >
+            </v-form>
+          </v-col>
+        </v-row></v-container
+      >
+    </footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "blog",
+  data() {
+    return {
+      drawer: false,
+      dialog: false,
+      dialog2: false,
+    };
+  },
+  computed: {
+    checkTransparent() {
+      if (document.body.clientWidth < 966) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,372 @@
+<template>
+  <div height="610">
+    <v-row class="justify-center d-none d-md-flex" dense>
+      <v-col cols="5">
+        <v-img width="610" style="margin-top: -64px">
+          <img src="@/assets/img/headerimg.jpg" id="home" alt="" />
+        </v-img>
+      </v-col>
+      <v-col cols="7">
+        <v-row class="justify-center" style="height: 103%">
+          <v-col xs="8" lg="8" justify-center align-self="end" align-center
+            ><h4
+              class="d-flex justify-center align-center mb-7"
+              :style="styleTitle"
+            >
+              Kembang Flower Mantap
+            </h4>
+
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p></v-col
+          >
+          <v-col
+            offset="2"
+            class="d-flex align-end"
+            align-self="end"
+            height="100%"
+          >
+            <v-btn
+              class="HeaderBtn justify-start text-left transparent elevation-0"
+              height="160"
+            >
+              <div class="d-block">
+                <p class="HeaderBtn__title">
+                  <v-icon class="pr-1">mdi-arrow-left</v-icon>Previous
+                </p>
+                <br />
+                <p
+                  class="text-capitalize font-weight-medium HeaderBtn__content"
+                  style="color: #156963"
+                >
+                  Kaktus Plant
+                </p>
+              </div>
+            </v-btn>
+            <v-btn
+              color="#156963"
+              class="HeaderBtn text-left justify-start"
+              height="160"
+            >
+              <div class="pl-2">
+                <p class="HeaderBtn__title" color="" style="color: #f2f2f2">
+                  next<v-icon class="pl-1">mdi-arrow-right</v-icon>
+                </p>
+                <br />
+                <p
+                  class="text-capitalize font-weight-medium white--text HeaderBtn__content"
+                >
+                  Kaktus Plant
+                </p>
+              </div>
+            </v-btn>
+          </v-col></v-row
+        >
+      </v-col>
+    </v-row>
+    <v-container>
+      <v-row class="mt-16">
+        <v-col cols="12">
+          <v-row class="servisesContent">
+            <template v-for="(element, i) in listServises">
+              <v-col cols="12" sm="6" md="3" tag="div" :key="i">
+                <v-hover
+                  v-slot="{ hover, prop }"
+                  open-delay="23"
+                  close-delay="123"
+                >
+                  <v-card
+                    v-bind="prop"
+                    style="border: 2px solid #e0e0e0"
+                    :color="hover ? '#e6e2e3' : undefined"
+                  >
+                    <v-row class="pa-6">
+                      <v-col cols="12">
+                        <v-row class="flex-rows">
+                          <v-col cols="3"
+                            ><v-img
+                              :src="
+                                require(`~/assets/icons/${element.icon}.svg`)
+                              "
+                            ></v-img
+                          ></v-col>
+                          <v-col cols="7" class="pl-3">
+                            <p
+                              v-if="checkLen(element.title)"
+                              class="servisesContent__title servisesContent__title-bigtext"
+                            >
+                              {{ element.title }}
+                            </p>
+
+                            <p
+                              v-else
+                              style="color: #156963"
+                              class="font-weight-bold text-subtitle servisesContent__title"
+                            >
+                              {{ element.title }}
+                            </p></v-col
+                          >
+                        </v-row>
+                      </v-col>
+                      <v-col cols="12"
+                        >Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Porro saepe eum eaque dignissimos</v-col
+                      >
+                    </v-row>
+                  </v-card></v-hover
+                ></v-col
+              >
+            </template>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-row class="pb-5">
+            <v-col class="align-self-end"
+              ><p class="text-h5 font-weight-medium">Featured Plants</p></v-col
+            >
+            <v-col cols="3" class="justify-end d-flex">
+              <v-autocomplete
+                dense
+                solo
+                hide-details="true"
+                append-icon="mdi-chevron-down"
+              ></v-autocomplete
+            ></v-col>
+          </v-row>
+          <v-divider></v-divider>
+
+          <v-col cols="12" lg="11" class="pa-0">
+            <v-list
+              class="d-flex list flex-wrap flex-lg-nowrap justify-sm-space-between justify-center"
+              dense
+            >
+              <v-card
+                flat
+                v-for="(el, i) in listFlowers"
+                :key="i"
+                class="pa-2"
+                style="width: 217px"
+              >
+                <v-row>
+                  <v-col cols="12" class="d-flex align-center">
+                    <v-img
+                      width="217px"
+                      :src="require(`@/assets/img/flowers/flower${i + 1}.png`)"
+                      alt="2"
+                    />
+                  </v-col>
+                  <v-col cols="12" class="text-center">
+                    <h3>{{ el.title }}</h3>
+                    <p>IDR {{ el.price }}</p>
+                  </v-col>
+                </v-row></v-card
+              >
+            </v-list>
+          </v-col>
+        </v-col>
+      </v-row>
+      <v-row class="justify-center justtify-md-start my-10">
+        <v-col cols="12" md="6">
+          <v-row class="align-items-center" style="height: 100%">
+            <v-col cols="12" md="10"
+              ><h4
+                class="font-weight-bold mb-8"
+                style="font-size: 44px; line-height: 1.2"
+              >
+                Buy more plants, it helps you to be relaxed
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                gravida gravida aliquam. Pellentesque et lobortis nisl. Sed et
+                mauris justo. Nulla eu enim non mauris maximus dignissim.
+                Maecenas vitae eros sapien. Quisque pellentesque tempus
+                dignissim.
+              </p></v-col
+            >
+            <v-col
+              cols="12"
+              md="11"
+              align-self="end"
+              class="pa-0"
+              style="margin-top: 10px"
+            >
+              <img
+                src="@/assets/img/background2.jpg"
+                alt="838"
+                style="width: 100%"
+                aspect-ratio=""
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col md="6" class="pa-0 pl-3 pb-6 d-none d-md-block">
+          <img
+            src="@/assets/img/background.jpg"
+            alt="img"
+            style="width: 100%"
+          />
+        </v-col>
+      </v-row>
+      <v-row
+        ><v-col cols="12" class="pl-0 pr-0"
+          ><div style="background: #f3f4f6" class="pa-16 pr-4 pb-0">
+            <v-row class="flex-column flex-sm-row">
+              <v-col cols="6" class="pl-7 flex-column"
+                ><h3 style="line-height: 1.2" class="mb-10 mt-7 text--md-h3">
+                  Get your favourites plant on our shop now
+                </h3>
+                <v-btn
+                  color="#156963"
+                  class="white--text text-body-2 text-capitalize"
+                  style="letter-spacing: 1.2px"
+                  @click="$vuetify.goTo('#home')"
+                  >Visit Shop</v-btn
+                ></v-col
+              >
+              <v-col cols="6" class="pa-0 d-flex justify-end">
+                <img
+                  class="pb-0"
+                  style="max-width: 100%"
+                  src="@/assets/img/background3.png"
+                  alt=""
+                />
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  layout: "blog",
+  data() {
+    return {
+      selectItem: false,
+      styleTitle: [
+        {
+          "font-size": "68px",
+          "font-weight": "500",
+
+          color: "#156963",
+          "font-family": "Montserrat",
+          "line-Height": "70px",
+        },
+      ],
+      listServises: [
+        { icon: "fastdelivery", title: "Fast Delivery" },
+        { icon: "headphones", title: "Great Customer " },
+        { icon: "plant", title: "Original Plants" },
+        { icon: "dollar-symbol", title: "Affordable Price" },
+      ],
+      listFlowers: [
+        {
+          img: "",
+          title: "Kaktus Plants",
+          price: 85.0,
+        },
+        {
+          img: "",
+          title: "Landak Plants",
+          price: 105.0,
+        },
+        {
+          img: "",
+          title: "Kecubung Plants",
+          price: 85.0,
+        },
+        {
+          img: "",
+          title: "Kecubung Plants",
+          price: 85.0,
+        },
+        {
+          img: "",
+          title: "Kecubung Plants",
+          price: 85.0,
+        },
+      ],
+    };
+  },
+  methods: {
+    checkLen(title) {
+      if (title.length > 17) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.HeaderBtn {
+  /* display: block;/ */
+  /* height:50%; */
+  justify-content: baseline;
+  width: 50%;
+  &__title {
+    font-size: 24px;
+    color: hsla(0, 0%, 95%, 1);
+    display: flex;
+    align-items: center;
+  }
+  &__content {
+    font-size: 32px;
+  }
+}
+p {
+  margin: 0;
+}
+.servisesContent {
+  &__title {
+    font-family: "Montserrat";
+    font-size: 18px;
+    // font-size: 24px;
+    &-bigtext {
+      font-size: 16px;
+      font-weight: bold;
+      color: #156963;
+      overflow: hidden;
+    }
+  }
+}
+.select {
+  width: 130px;
+  border: 1px solid #940;
+  padding: 0 15px;
+  text-align: center;
+  position: relative;
+  display: flex;
+  cursor: pointer;
+  ul {
+    display: none;
+    margin-top: 20px;
+  }
+  &::after {
+    content: "\\/";
+    // width: 90px;
+    // height: 90px;
+    // position: absolute;
+    // background: #444;
+    margin-left: auto;
+  }
+}
+.select.active {
+  ul {
+    display: block;
+  }
+  &::after {
+    content: "/\\";
+  }
+}
+</style>
